d9846c3642b1c3c9fa43c4173a8238a3abc91a0a
Justin Acance <jacance@Justins-MacBook-Pro.local>
2020-12-13 14:43:37 -0800

    jupyter notebook solved

--- EmployeeSQL/pewlett_hackard_queries.sql
+++ EmployeeSQL/pewlett_hackard_queries.sql
@@ -66,14 +66,14 @@ REFERENCES "employees" ("emp_no");
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
 INNER JOIN salaries ON employees.emp_no=salaries.emp_no
-
+;
 
 --List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date >= '19860101' AND hire_date < '19870101'
 ORDER BY last_name ASC
-
+;
 
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
@@ -82,7 +82,7 @@ INNER JOIN departments AS d ON
 dm.dept_no = d.dept_no
 INNER JOIN employees AS e ON
 e.emp_no = dm.emp_no
-
+;
 
 --List the department of each employee with the following information: employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
@@ -97,7 +97,7 @@ d.dept_no = de.dept_no
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
-
+;
 
 --List all employees in the Sales department, including their employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
@@ -107,7 +107,7 @@ e.emp_no = de.emp_no
 INNER JOIN departments AS d ON
 d.dept_no = de.dept_no
 WHERE dept_name = 'Sales'
-
+;
 
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
@@ -117,10 +117,11 @@ e.emp_no = de.emp_no
 INNER JOIN departments AS d ON
 d.dept_no = de.dept_no
 WHERE dept_name = 'Sales' OR dept_name = 'Development'
-
+;
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 SELECT last_name, COUNT(emp_no)
 FROM employees
 GROUP BY last_name
 ORDER BY count DESC
+;
