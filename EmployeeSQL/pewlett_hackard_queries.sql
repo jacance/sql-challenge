55393fb248b176666eda08d5e15d1494e27de892
Justin Acance <jacance@Justins-MacBook-Pro.local>
2020-12-13 14:51:51 -0800

    clean up query

--- EmployeeSQL/pewlett_hackard_queries.sql
+++ EmployeeSQL/pewlett_hackard_queries.sql
@@ -91,7 +91,7 @@ INNER JOIN dept_emp AS de ON
 e.emp_no = de.emp_no
 INNER JOIN departments AS d ON
 d.dept_no = de.dept_no
-
+;
 
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name, sex
